713b351d91c7ee3d813b5f5aeb5957df15cce82a
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-10-16 23:18:06 -0700

    removes logging

--- src/MentionsInput.js
+++ src/MentionsInput.js
@@ -575,14 +575,12 @@ class MentionsInput extends React.Component {
     if(!this.refs.input || !this.refs.highlighter) {
       // since the invocation of this function is deferred,
       // the whole component may have been unmounted in the meanwhile
-      console.log(`updateHighlighterScroll can't update`);
       return;
     }
     const input = ReactDOM.findDOMNode(this.refs.input);
     const highlighter = ReactDOM.findDOMNode(this.refs.highlighter);
     highlighter.scrollLeft = input.scrollLeft;
     highlighter.scrollTop = input.scrollTop;
-    console.log(input.scrollTop);
   };
 
   handleCompositionStart = () => {
